36c7264f867b9ada38fa7d7b665bd99d7d857a67
Hassan-Adelani-Luqman <h.luqman@alustudent.com>
2025-05-24 17:22:49 +0200

    re-factored the code for PEP8 validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
+
 """
-Module that contains min_operations function.
+Given a number n, write a method that calculates the fewest # of operations.
 
-Given a number n, this function calculates the fewest number
-of operations needed to achieve exactly n 'H' characters.
-
-Prototype: def min_operations(n)
-Returns an integer.
-If n is impossible to achieve, returns 0.
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
 """
 
 
-def min_operations(n):
-    """
-    Calculates the fewest number of operations to get n H characters.
-
-    Args:
-        n (int): The target number of H characters.
-
-    Returns:
-        int: The minimum number of operations needed.
-             Returns 0 if n is impossible to achieve.
-    """
+def minOperations(n):
     if n <= 1:
         return 0
 
     i = 2
     while i * i <= n:
         if n % i == 0:
-            return i + min_operations(n // i)
+            return i + minOperations(n // i)
         i += 1
 
     return n
